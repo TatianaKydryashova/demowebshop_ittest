ba6812ebc0589724a3e96da4d80e1977fd5dbb9c
TatianaKydryashova <kudryashova_ts@mail.ru>
2022-03-31 09:13:24 +0300

    Add class TestLoginUserPage

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,7 +1,11 @@
+import pytest
+
+from data.CreateUser import User
 from pages.LoginPage import LoginPage
+from pages.RegisterPage import RegisterPage
 
 
-def test_register_new_user(browser):
+def test_login_user(browser):
     link = "http://demowebshop.tricentis.com/login"
     page = LoginPage(browser, link)
     page.open()
@@ -9,3 +13,20 @@ def test_register_new_user(browser):
     Password = "112233441"
     page.login_user(Email, Password)
     page.should_be_user_page()
+
+
+class TestLoginUserPage():
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://demowebshop.tricentis.com/register"
+        page = RegisterPage(browser, link)
+        page.open()
+        page.register_new_faker_user(User)
+
+    def test_login(self, browser):
+        link = "http://demowebshop.tricentis.com/login"
+        page = LoginPage(browser, link)
+        page.open()
+        page.login_user(User.EMAIL, User.PASSWORD)
+        page.should_be_user_page()
