18dfe47f099930fb4ab745913dcdd1fe64c28329
TatianaKydryashova <kudryashova_ts@mail.ru>
2022-03-31 12:09:25 +0300

    addoption browser_name and headless

--- conftest.py
+++ conftest.py
@@ -1,20 +1,32 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--headless', action="store_true", help="Run driver in headless mode.")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    headless = request.config.getoption("headless")
     browser = None
     if browser_name == "chrome":
-        browser = webdriver.Chrome()
+        if not headless:
+            browser = webdriver.Chrome()
+        else:
+            options = Options()
+            options.add_argument("--headless")
+            browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        browser = webdriver.Firefox()
+        if not headless:
+            browser = webdriver.Firefox()
+        else:
+            options = Options()
+            options.add_argument("-headless")
+            browser = webdriver.Firefox(options=options)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
